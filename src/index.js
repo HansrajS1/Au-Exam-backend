af5e47b43a9b45c69f1b5bf38e6c18e89c152dac
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2025-09-28 00:17:25 +0530

    fix port

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ const cors = require('cors');
 require('dotenv').config();
 
 const paperRoutes = require('./routes/paperRoutes');
+const db = require('./config/db');
 
 const app = express();
-app.use(cors({ origin: ['http://localhost:5173', 'https://auexamweb.netlify.app'] }));
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://auexamweb.netlify.app']
+}));
+
 app.use(express.json());
 
 app.use('/api/papers', paperRoutes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
-
-const db = require('./config/db');
-
-
 app.get('/', (req, res) => {
-  res.send(' API is running');
+  res.send('API is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
