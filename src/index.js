6d93b06b6d612aa1216a28fc614fe4cf4f616a25
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2025-09-29 10:54:22 +0530

    add health and error router

--- src/index.js
+++ src/index.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
+app.get('/health', (req, res) => res.send('OK'));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
