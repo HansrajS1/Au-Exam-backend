7ebbe165172751369a007928230c01c7cc3020bc
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2025-10-12 02:32:02 +0530

    updating with openai api askaipage

--- src/config/db.js
+++ src/config/db.js
@@ -2,14 +2,32 @@ require('dotenv').config();
 const knex = require('knex');
 
 const db = knex({
-  client: 'pg',
-  connection: process.env.POSTGRES_URI,
-  searchPath: ['public'],
-  pool: {
-    min: 5,
-    max: 25,
-    acquireTimeoutMillis: 30000
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",         
+    port: 5432,
+    user: "postgres",           
+    password: "admin",  
+    database: "au_exam"
   }
 });
+
+module.exports = db;
+
+
+
+// require('dotenv').config();
+// const knex = require('knex');
+
+// const db = knex({
+//   client: 'pg',
+//   connection: process.env.POSTGRES_URI,
+//   searchPath: ['public'],
+//   pool: {
+//     min: 5,
+//     max: 25,
+//     acquireTimeoutMillis: 30000
+//   }
+// });
 
 module.exports = db;
